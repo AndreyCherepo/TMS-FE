3740cb25b1b7ebb63eb35e12ed64093eaa64c24e
AndreyCherepo <andreyandrey8967@gmail.com>
2022-11-04 18:46:30 +0300

    f

---
+++ webpack-boilerplates /src/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+})
